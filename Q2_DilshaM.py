cde6a7463e4c965600cb1f20c2c2ad141a6d7075
Dilsha <dilsham998@gmail.com>
2021-07-05 17:33:29 +0530

    intialing

--- Q2_DilshaM.py
+++ Q2_DilshaM.py
@@ -1,5 +1,9 @@
-rows=int(input("Enter the number of rows:"))
-for i in range(1,rows+1):
-    for j in range(1,i+1):
-        print(i*j,end=' ')
+num=int(input("Enter the number of rows:"))
+for i in range(num):
+    value =i+1
+    dec=num-1
+    for j in range(i+1):
+        print(format(value,"<4"),end=' ')
+        value =value+dec
+        dec=dec-1
     print()
--- Question1.py
+++ Question1.py
@@ -1,33 +1,26 @@
 def printDiamond(n):
     print("")
     j = 0
-
     for i in range(0, n):
-
         for j in range(i, n):
-            print("*", end="")
-
+            print("*", end=" ")
         for j in range(0, 2 * i + 1):
-            print(" ", end="")
-
+            print(" ", end=" ")
         for j in range(i, n):
-            print("*", end="")
-
-        print("")
-
+            print("*", end=" ")
+        print()
     for i in range(0, n - 1):
         for j in range(0, i + 2):
-            print("*", end="")
-
+            print("*", end=" ")
         for j in range(0, 2 * (n - 1 - i) - 1):
-            print(" ", end="")
+            print(" ", end=" ")
 
         for j in range(0, i + 2):
-            print("*", end="")
+            print("*", end=" ")
 
-        print("")
+        print()
 
-    print("")
+    print()
 
 
 if __name__ == '__main__':
@@ -35,3 +28,20 @@ if __name__ == '__main__':
     print("Inverse Diamond Pattern for n = ", n)
     printDiamond(n)
 
+# n = int(input("enter the num of rows:"))
+# for i in range(n,0,-1):
+#     for j in range(i,0,-1):
+#         print("*",end=" ")
+#     for j in range(2*(n-i)):
+#         print(" ",end=" ")
+#     for j in range(i,0,-1):
+#         print("*",end=" ")
+#     print()
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     for j in range(2*(n-i-1)):
+#         print(" ", end=" ")
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
