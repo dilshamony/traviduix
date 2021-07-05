a2b4b7f56f2bb262a74a40e6823f97ed481b157d
Dilsha <dilsham998@gmail.com>
2021-07-05 16:05:58 +0530

    inti

--- Question1.py
+++ Question1.py
@@ -7,11 +7,9 @@ def printDiamond(n):
         for j in range(i, n):
             print("*", end="")
 
-        # for middle " "
         for j in range(0, 2 * i + 1):
             print(" ", end="")
 
-        # for right *
         for j in range(i, n):
             print("*", end="")
 
@@ -24,7 +22,6 @@ def printDiamond(n):
         for j in range(0, 2 * (n - 1 - i) - 1):
             print(" ", end="")
 
-        # for right *
         for j in range(0, i + 2):
             print("*", end="")
 
@@ -34,7 +31,6 @@ def printDiamond(n):
 
 
 if __name__ == '__main__':
-    # Define n
     n = 10
     print("Inverse Diamond Pattern for n = ", n)
     printDiamond(n)
